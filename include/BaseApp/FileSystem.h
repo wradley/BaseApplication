850777e56a494711c1402361dbe23f4d79723031
Whelan Callahan <wcallahan@wi.rr.com>
2018-04-04 16:56:42 -0500

    read/write text files

--- include/BaseApp/FileSystem.h
+++ include/BaseApp/FileSystem.h
@@ -8,5 +8,8 @@ namespace App
 	{
 		ByteBuffer ReadBytes(const std::string &file);
 		void WriteBytes(const std::string &file, const ByteBuffer &b);
+
+		std::string ReadText(const std::string &file);
+		void WriteText(const std::string &file, const std::string &text);
 	}
 }
