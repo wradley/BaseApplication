3544394ec06eb6e3af6f834c83eb673860f6cf98
Whelan Callahan <wcallahan@wi.rr.com>
2018-04-04 16:56:12 -0500

    read/write text files

--- src/FileSystem.cpp
+++ src/FileSystem.cpp
@@ -6,10 +6,17 @@
 App::ByteBuffer App::FileSystem::ReadBytes(const std::string &filename)
 {
 	ByteBuffer b;
-	
+
 	//open file
 	std::ifstream infile;
 	infile.open(filename, std::ios::binary | std::ios::in);
+
+	// check if file was opened
+	if (!infile.is_open())
+	{
+		std::cout << "Could not open file: " << filename << std::endl;
+		return b;
+	}
 
 	//get length of file
 	infile.seekg(0, infile.end);
@@ -21,7 +28,6 @@ App::ByteBuffer App::FileSystem::ReadBytes(const std::string &filename)
 	infile.read((char*)b.head(), length);
 
 	infile.close();
-
 	return b;
 }
 
@@ -33,6 +39,36 @@ void App::FileSystem::WriteBytes(const std::string &filename, const ByteBuffer &
 		std::ofstream ofile;
 		ofile.open(filename, std::ios::binary | std::ios::out);
 		ofile.write((char*)b.head(), b.size());
+		ofile.close();
+	}
+}
+
+
+std::string App::FileSystem::ReadText(const std::string & file)
+{
+	std::ifstream ifs(file);
+
+	// check if file was opened
+	if (!ifs.is_open())
+	{
+		std::cout << "Could not open file: " << file << std::endl;
+		return std::string();
+	}
+
+	std::string content((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
+
+	ifs.close();
+	return content;
+}
+
+
+void App::FileSystem::WriteText(const std::string & file, const std::string & text)
+{
+	if (text.size() > 0)
+	{
+		std::ofstream ofile;
+		ofile.open(file);
+		ofile << text;
 		ofile.close();
 	}
 }
